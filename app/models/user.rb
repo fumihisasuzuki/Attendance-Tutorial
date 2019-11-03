a34fc11495b9fa6b158310851a50577c222854d1
Fumihisa Suzuki <fumi_s_qrio192@yahoo.co.jp>
2019-11-03 14:28:01 +0000

    9.end Finish add-basic-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # 「remember_token」という仮想の属性を作成します。
   attr_accessor :remember_token
   
   before_save { self.email = email.downcase }
@@ -9,6 +10,8 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true 
   validates :department, length: { in: 2..50 }, allow_blank: true
+  validates :basic_time, presence: true
+  validates :work_time, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
